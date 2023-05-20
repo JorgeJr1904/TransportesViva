75e4bde78efc90e491953fe5319acdea1c083e74
JorgeJr <jorjaispro@gmail.com>
2023-05-19 22:30:16 -0600

    Correccion del Cross Origin

--- src/main/java/me/transportesviva/restApi/RestApiApplication.java
+++ src/main/java/me/transportesviva/restApi/RestApiApplication.java
@@ -2,6 +2,9 @@ package me.transportesviva.restApi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class RestApiApplication {
@@ -10,4 +13,19 @@ public class RestApiApplication {
 		SpringApplication.run(RestApiApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("http://localhost:8080")
+						.allowedMethods("GET", "POST", "PUT", "DELETE")
+						.allowedHeaders("*");
+			}
+		};
+	}
+
 }
+
+
