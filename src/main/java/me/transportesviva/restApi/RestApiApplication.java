70e569f7c9044e3e833de7a66bf48d2795aa4953
JorgeJr <jorjaispro@gmail.com>
2023-05-19 22:37:02 -0600

    h

--- src/main/java/me/transportesviva/restApi/RestApiApplication.java
+++ src/main/java/me/transportesviva/restApi/RestApiApplication.java
@@ -19,7 +19,7 @@ public class RestApiApplication {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
-						.allowedOrigins("http://localhost:8080")
+						.allowedOrigins("*")
 						.allowedMethods("GET", "POST", "PUT", "DELETE")
 						.allowedHeaders("*");
 			}
